ac7b840bcf4f263a24ed8da35e7fa6f6b8d4a8b2
stowcoal <cdstochl@gmail.com>
2014-11-04 22:52:59 -0600

    add more querying and such

--- public_html/test.php
+++ public_html/test.php
@@ -9,5 +9,24 @@ $result = mysqli_query($con, "select * from bars");
 while($row = mysqli_fetch_array($result)) {
   echo $row['name'] . " " . $row['address'];
   echo "<br>";
+  $specials = mysqli_query($con, "select * from specials");
+?>
+<table>
+	<tbody>
+<?php
+  while($special_row = mysqli_fetch_array($specials)) 
+  {
+?>
+<tr>
+	<td><?php echo($special_row['name']);?></td>
+	<td><?php echo($special_row['price']);?></td>
+	<td><?php echo($special_row['item']);?></td>
+</tr>
+<?php
+  } 
+?>
+</tbody>
+</table>
+<?php
 }
 ?>
